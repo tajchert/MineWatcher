bc264e5b927db8d06f30763d13ccfd76192fc716
Michal Tajchert <thetajchert@gmail.com>
2013-10-27 13:28:27 +0100

    Deleted unnecessary info

--- MineWatcher/Defaults.cs
+++ MineWatcher/Defaults.cs
@@ -9,8 +9,7 @@ namespace MineWatcher
     {
         public static string POLMINE_ADDRESS = "https://polmine.pl/?action=api&cmd=";
         //https://polmine.pl/?action=api&cmd=dc48a571da3d78b1e744d14d83355729
-        //THEIR 2e58e39abfa62691501af17301128a98
-        //MY dc48a571da3d78b1e744d14d83355729
+        //Test one 2e58e39abfa62691501af17301128a98
         public static int MINUTES_TO_OFFLINE = 30;
         public static bool SHOW_PERCENTAGE = true;
     }
